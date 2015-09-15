05b867c307b0cb41e65bf4aea9e3bf2a5f42f2e4
Penghua Zhao <phzhao@umich.edu>
2015-09-15 19:08:11 -0400

    add createtable.sql

---
+++ pa1/sql/tlb_create.sql
@@ -0,0 +1,37 @@
+CREATE TABLE User(
+       username VARCHAR(20),
+       firstname VARCHAR(20),
+       lastname VARCHAR(20),
+       password VARCHAR(40),
+       email VARCHAR(40),
+       PRIMARY KEY (username)
+);
+
+CREATE TABLE Album (
+       albumid int AUTO_INCREMENT,
+       title varchar(50),
+       created date,
+       lastupdated date,
+       username varchar(20),
+       FOREIGN KEY (username) REFERENCES User(username),
+       PRIMARY KEY (albumid)
+);
+
+
+CREATE TABLE Contain (
+       albumid int,
+       picid varchar(40),
+       caption varchar(255),
+       sequencenum int,
+       FOREIGN KEY (albumid) REFERENCES Album(albumid),
+       FOREIGN KEY (picid) REFERENCES Photo(picid),
+       PRIMARY KEY (albumid, picid)
+);
+
+CREATE TABLE Photo (
+       picid varchar(40),
+       url varchar(255),
+       format char(3),
+       date date,
+       PRIMARY KEY (picid)
+);
