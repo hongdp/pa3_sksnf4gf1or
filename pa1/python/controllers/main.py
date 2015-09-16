037d8aa0db3efac88b188b725255ecac74067093
Dapeng Hong <hongdp@gmail.com>
2015-09-16 19:24:31 -0400

    use ./ instead of / in href

--- pa1/python/controllers/main.py
+++ pa1/python/controllers/main.py
@@ -9,4 +9,4 @@ def main_route():
 	cur = mysql.connection.cursor()
 	cur.execute("SELECT username FROM User")
 	msgs = cur.fetchall()
-	return render_template("index.html", usernames=msgs, secretKey=secretKey)
+	return render_template("index.html", usernames=msgs)
