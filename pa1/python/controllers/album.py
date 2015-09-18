7489a277e61c9c0292c5bd42f2d5cf42f56ef544
Dapeng Hong <hongdp@gmail.com>
2015-09-18 14:53:35 -0400

    front end for /album/edit

--- pa1/python/controllers/album.py
+++ pa1/python/controllers/album.py
@@ -3,10 +3,20 @@ from flask import *
 
 album = Blueprint('album', __name__, template_folder='views')
 
-@album.route(appendKey('/album/edit'))
+@album.route(appendKey('/album/edit'), methods = ['GET', 'POST'])
 def album_edit_route():
+	albumid = request.args.get('id')
+	cur = mysql.connection.cursor()
+	cur.execute("SELECT Photo.picid, url FROM Photo, Contain WHERE Photo.picid = Contain.picid AND Contain.albumid = '%s' ORDER BY sequencenum "%(albumid))
+	photos = cur.fetchall()
+	cur.execute("SELECT username, title FROM Album WHERE albumid = '%s'" %(albumid))
+	albumInfo = cur.fetchall()
 	options = {
-		"edit": True
+		"edit": True,
+		"photos": photos,
+		"username": albumInfo[0][0],
+		"albumname":albumInfo[0][1],
+		"albumid": albumid,
 	}
 	return render_template("album.html", **options)
 
@@ -23,7 +33,8 @@ def album_route():
 		"edit": False,
 		"photos": photos,
 		"username": albumInfo[0][0],
-		"albumname":albumInfo[0][1]
+		"albumname":albumInfo[0][1],
+		"albumid": albumid,
 	}
 	print options
 	return render_template("album.html", **options)
