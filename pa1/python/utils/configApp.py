768c52f2edc295c4ddce8343eade194a330752eb
hongdp <hongdp@umich.edu>
2015-09-20 16:52:11 -0400

    add configApp

---
+++ pa1/python/utils/configApp.py
@@ -0,0 +1,5 @@
+def configApp(app):
+	app.config['MYSQL_USER'] = 'group88'
+	app.config['MYSQL_PASSWORD'] = 'awesomejph'
+	app.config['MYSQL_DB'] = 'group88'
+	app.config['MYSQL_HOST'] = 'localhost'
