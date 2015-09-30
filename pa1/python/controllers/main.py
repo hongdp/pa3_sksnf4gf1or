1850567a4e18d63bf7965b25d4c724e8f66d2d7c
Penghua Zhao <phzhao@umich.edu>
2015-09-29 20:05:55 -0400

    try login

--- pa1/python/controllers/main.py
+++ pa1/python/controllers/main.py
@@ -10,3 +10,18 @@ def main_route():
 	cur.execute("SELECT username FROM User")
 	msgs = cur.fetchall()
 	return render_template("index.html", usernames=msgs)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return ''
+
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
