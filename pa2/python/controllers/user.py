00028cce2002c3749e74a65e5032d098cfb1ba58
Dapeng Hong <hongdp@gmail.com>
2015-10-05 20:11:08 -0400

    front end of user/edit

--- pa2/python/controllers/user.py
+++ pa2/python/controllers/user.py
@@ -39,3 +39,35 @@ def reg():
 
 
     return render_template('user.html')
+
+@user.route(appendKey('/user'), methods=['POST'])
+def deleteUser():
+    # redirect to user/edit
+    # if request.method == 'GET':
+    #   if 'username' in session:
+    #       return redirect(url)
+    if not sessionExists(session):
+        return render_template("noLogin.html")
+    elif sessionIsExpired(session):
+        session.clear()
+        return render_template("sessionExpire.html")
+
+    username = session['username']
+    con = mysql.connection
+    cur = con.cursor()
+    cur.execute("SELECT * FROM User WHERE username='%s'"%(username))
+    exist = cur.fetchall()
+    if not exist:
+        abort(404)
+    cur.execute("SELECT Photo.picid FROM User, Album, Contain, Photo WHERE User.username = Album.username AND Album.albumid = Contain.albumid AND Contain.picid = Photo.picid")
+    if request.form['password'] != request.form['re-password']:
+        error = 'password does not match'
+        return render_template('user.html',error=error)
+
+    hash_password = hashlib.sha224(request.form['password']).hexdigest()
+    cur.execute("INSERT INTO User VALUES ('%s', '%s', '%s', '%s', '%s')" % (request.form['username'], \
+    request.form['firstname'], request.form['lastname'],hash_password , request.form['email']) )
+    session['username'] = request.form['username']
+    renewSession(session)
+    con.commit()
+    return redirect(url_for('main.main_route'))
