039e188ea3d4f03df90d055c2bd747a7a7e756ec
Penghua Zhao <phzhao@umich.edu>
2015-10-22 20:51:26 -0400

    part2a

--- pa3/python/static/js/caption.js
+++ pa3/python/static/js/caption.js
@@ -1,5 +1,29 @@
 
 
+function Caption(element, picid, caption) {
+  this.element = element;
+  this.picid = picid;
+  element.value = caption; // objects in Javascript are assigned by reference, so this works
+  element.addEventListener("change", this, false);
+}
+
+Caption.prototype.handleEvent = function(e) {
+  if (e.type === "change") {
+    this.update(this.element.value);
+  }
+}
+
+Caption.prototype.change = function(value) {
+  this.data = value;
+  this.element.value = value;
+}
+
+Caption.prototype.update = function(caption) {
+  makeCaptionPostRequest(this.picid, caption, function() {
+    console.log('POST successful.');
+  });
+}
+
 function makeCaptionRequest(picid, cb) {
   qwest.get('/secretkey/pa3/pic/caption?id=' + picid)
     .then(function(xhr, resp) {
